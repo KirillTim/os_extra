06c3016618f5ed1841fb08a76087f1843491920f
Kirill <timofeev.kirill.a@gmail.com>
2016-04-03 01:33:46 +0300

    add parse command test and fix typos

--- tests.h
+++ tests.h
@@ -5,18 +5,18 @@ using namespace std;
 
 typedef vector<string> program;
 
-void print_diff(const vector<string>& expected, const vector<string>& actual) {
-    cerr<<"expected:\n";
-    for (auto& i : expected)
-        cerr<<i<<"\n";
-    cerr<<"actual:\n";
-    for (auto& i : actual)
-        cerr<<i<<"\n";
+void print_diff(const vector<string> &expected, const vector<string> &actual) {
+    cerr << "expected:\n";
+    for (auto &i : expected)
+        cerr << i << "\n";
+    cerr << "actual:\n";
+    for (auto &i : actual)
+        cerr << i << "\n";
 }
 
-void test_parse_args_ok(const string line, const vector<string>& ans) {
+void test_parse_args_ok(const string line, const vector<string> &ans) {
     vector<string> prog;
-    int res = parse_args((char*)line.c_str(), line.size(), prog);
+    int res = parse_args((char *) line.c_str(), line.size(), prog);
     if (res != 1) {
         cerr << "res != -1\n";
         return;
@@ -31,19 +31,57 @@ void test_parse_args_ok(const string line, const vector<string>& ans) {
             return;
         }
     }
-    cerr<<"OK\n";
+    cerr << "test_parse_args_ok(" << line << ") -- OK\n";
+}
+
+void test_parse_command_ok(const string line, const vector<vector<string>> &ans) {
+    vector<program> progs;
+    int res = parse_command((char *) line.c_str(), line.size(), progs);
+    if (res != 1) {
+        cerr << "res != 1\n";
+        return;
+    }
+    if (ans.size() != progs.size()) {
+        cerr << "expected:\n";
+        for (auto &i :ans) {
+            for (auto &j : i)
+                cerr << j << " ";
+            cerr << "\n";
+        }
+        cerr << "actual:\n";
+        for (auto &i :progs) {
+            for (auto &j : i)
+                cerr << j << " ";
+            cerr << "\n";
+        }
+        return;
+    }
+    for (int i = 0; i < ans.size(); i++) {
+        if (ans[i].size() != progs[i].size()) {
+            print_diff(ans[i], progs[i]);
+            return;
+        }
+        for (int j = 0; j < ans[i].size(); j++) {
+            if (ans[i][j] != progs[i][j]) {
+                print_diff(ans[i], progs[i]);
+                return;
+            }
+        }
+    }
+    cerr << "test_parse_command_ok(" << line << ") -- OK\n";
 }
 
 void test_parse_args_fail(const string line) {
     vector<string> prog;
-    if (parse_args((char*)line.c_str(), line.size(), prog) != 0)
-        cerr<<"for line: "<<line<<" 0 expected\n";
+    if (parse_args((char *) line.c_str(), line.size(), prog) != 0)
+        cerr << "for line: " << line << " 0 expected\n";
     else
-        cerr<<"OK\n";
+        cerr << "OK\n";
 
 }
 
 void test_all() {
+    test_parse_args_ok("uniq", {"uniq"});
     test_parse_args_ok("cat /proc/cpuinfo", {"cat", "/proc/cpuinfo"});
     test_parse_args_ok("grep 'model name'", {"grep", "'model name'"});
     test_parse_args_ok("sed    -re    's/.*: (.*)/\\1/'", {"sed", "-re", "'s/.*: (.*)/\\1/'"});
