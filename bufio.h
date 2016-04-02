2c542971984834d6a9270164ec57d729e7f3045d
Kirill <timofeev.kirill.a@gmail.com>
2016-04-02 16:01:03 +0300

    fix comments

--- bufio.h
+++ bufio.h
@@ -7,10 +7,12 @@ struct buf_t {
 public:
     buf_t();
     ~buf_t();
+
     size_t size;
     char* data;
-    //read from non-block socket
-    //return -1 on error, 0 on file close , bytes read otherwise
+
+    /*read from non-block socket.
+     return -1 on error, 0 on file close , bytes read otherwise */
     int read_all(int fd);
 };
 
