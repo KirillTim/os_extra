1509f297ef13d4448d9a2259f18139a437eb33cc
Kirill <timofeev.kirill.a@gmail.com>
2016-04-03 01:14:51 +0300

    add command parser

--- parsers.h
+++ parsers.h
@@ -47,5 +47,24 @@ int parse_args(char *args, size_t len, program& prog) {
     return 1;
 }
 
+int parse_command(char* args, size_t len, vector<program>& progs) {
+    int pos = 0;
+    for (pos; pos < len; pos++)
+        if (args[pos] == '\n')
+            break;
+    if (pos == len)
+        return 0;
+    int st = 0;
+    for (int i = 0; i < pos; i++) {
+        if (args[i] == '|') {
+            program cur;
+            if (parse_args(args+st, (size_t)i-st, cur) <= 0)
+                return -1; //can't parse args
+            progs.push_back(cur);
+        }
+    }
+    return 1;
+}
+
 
 #endif //OS_EXTRA_PARSERS_H
