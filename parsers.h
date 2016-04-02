511110e4ebc811527ea05d896cfd4321ddda4343
Kirill <timofeev.kirill.a@gmail.com>
2016-04-03 00:25:41 +0300

    first parser add

---
+++ parsers.h
@@ -0,0 +1,45 @@
+#ifndef OS_EXTRA_PARSERS_H
+#define OS_EXTRA_PARSERS_H
+
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef vector<string> program;
+
+int parse_args(char* args, int len, program& prog) {
+    if (len <= 0)
+        return -1;
+    int first = 0;
+    while (args[first] == ' ')
+        first++;
+    while (args[len-1] == ' ')
+        len --;
+    bool double_quote = false, quote = false;
+    int st = -1;
+    for (int i = first; i < len; i++) {
+        if (args[i] != ' ') {
+            if (st == -1)
+                st = i;
+        }
+        if (args[i] == '\'')
+        if (!double_quote)
+            quote = !quote;
+        if (args[i] == '\"')
+        if (!quote)
+            double_quote = !double_quote;
+        if (args[i] == '\\') {
+            i++; //just skip one symbol
+        }
+        if (args[i] == ' ' && st != -1 && !(double_quote || quote)) {
+            prog.push_back(string(args + st, (size_t) (i - st)));
+            st = -1;
+        }
+    }
+    if (quote || double_quote) //braces not paired
+        return 0;
+    return 1;
+}
+
+
+#endif //OS_EXTRA_PARSERS_H
