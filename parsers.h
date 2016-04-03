80b7c4a79dbb4b98281d64a71745678aca8f8210
Kirill <timofeev.kirill.a@gmail.com>
2016-04-03 15:43:00 +0300

    now parser return execargs_t

--- parsers.h
+++ parsers.h
@@ -1,14 +1,16 @@
 #ifndef OS_EXTRA_PARSERS_H
 #define OS_EXTRA_PARSERS_H
 
-#include <string>
+#include <string.h>
+
 #include <vector>
+
+#include "helper.h"
 
 using namespace std;
 
-typedef vector<string> program;
-
-int parse_args(char *args, size_t len, program& prog) {
+int parse_args(char *args, size_t len, execargs_t& prog) {
+    vector<char* > temp;
     if (len == 0)
         return -1;
     int first = 0;
@@ -36,19 +38,31 @@ int parse_args(char *args, size_t len, program& prog) {
         }
         if (args[i] == ' ' && !(double_quote || quote)) {
             if (st != -1) {
-                prog.push_back(string(args + st, (size_t) (i - st)));
+                size_t sz = (size_t)(i - st);
+                char* str = (char*)malloc((sz+1));
+                memcpy(str, args+st, sz);
+                str[sz] = 0;
+                temp.push_back(str);
                 st = -1;
             }
         }
     }
     if (quote || double_quote) //braces not paired
         return 0;
-    prog.push_back(string(args + st, (size_t) (len - st)));
+
+    size_t sz = (size_t)(len - st);
+    char* str = (char*)malloc((sz+1));
+    memcpy(str, args+st, sz);
+    str[sz] = 0;
+    temp.push_back(str);
+
+    prog = execargs_from_vector(temp);
+
     return 1;
 }
 
 //TODO: fix parsing and pass all test
-int parse_command(char* args, size_t len, vector<program>& progs) {
+int parse_command(char* args, size_t len, vector<execargs_t >& progs) {
     int pos = 0;
     for (pos; pos < len; pos++)
         if (args[pos] == '\n')
@@ -71,7 +85,7 @@ int parse_command(char* args, size_t len, vector<program>& progs) {
         }
         if (args[i] == '|' && !(double_quote || quote)
                 || (args[i] != '|' && i == pos-1)) {
-            program cur;
+            execargs_t cur;
             if (i == pos - 1)
                 i ++;
             if (parse_args(args+st, (size_t)i-st, cur) <= 0)
